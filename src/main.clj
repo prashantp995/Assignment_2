78dff8626a2523c5a8273c58f2285713b714a5e3
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-05-25 16:48:23 -0400

    WIP 2D array

--- src/main.clj
+++ src/main.clj
@@ -1,6 +1,35 @@
-(ns main)
+(ns main
+  (:require [clojure.string :as str])
+  (:require [clojure.pprint :as pp]))
+
 (use 'clojure.java.io)
-(with-open [read (reader "map.txt")]
-  (doseq [line (line-seq read)]
-    (if (re-find #"@" line) (print line)))
-  )
+(def user)
+(def myVec [])
+(def row 0)
+(def column 0)
+(defn ppri2nt [the-array]
+  (doseq [a the-array]
+    (print a)
+    ))
+
+(defn treasure_hunt []
+  (with-open [rdr (reader "map.txt")]
+    (doseq [line (line-seq rdr)]
+      (def row (+ row 1))
+      (def column 0)
+      (doseq [character (str/split line #"")]
+        (def myVec (alength (to-array-2d [line])))
+        (def column (+ column 1))
+        (println character)))
+    (let [the-array (make-array Long/TYPE row column)]
+      (dotimes [nn (* row column)]
+        (let [ii (quot nn row)
+              jj (rem nn column)]
+          (aset the-array ii jj nn)
+          ))
+      (pp/pprint the-array)
+      (pp/pprint row)
+      (pp/pprint column)
+      )))
+
+(treasure_hunt)
